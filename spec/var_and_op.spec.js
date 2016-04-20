d31bdc72b0d6d86459812bad356a214e9d3c9550
melanie <mlanglois@simplon.co>
2016-04-20 16:08:07 +0200

    adding the content

---
+++ spec/var_and_op.spec.js
@@ -0,0 +1,39 @@
+var tester = require("../lib/var_and_op");
+
+
+describe("Javascript Variables and Operateur Kata", function() {
+
+	it("should check variables equality", function() {
+
+		var result = tester.isEquals(13, "13");
+
+		expect(result).toBeFalsy();
+	});
+
+
+	it("should add 10 and multiply by 5", function() {
+
+		var result = tester.add10AndMultiplyBy5(2);
+		expect(result).toBe(60);
+	})
+
+	it("should compute an addition and not a concatenation", function() {
+
+		var result = tester.sum(2, "5", 4);
+		expect(result).toBe(11);
+
+	});
+
+	it("should compute exponentiel", function() {
+		var result = tester.exponentiel(3, 2);
+		expect(result).toBe(8);
+	})
+
+
+    it("should compute the double of the number", function(){
+    	var result = tester.computeDouble(4);
+    	expect(result).toBe(8);
+
+    })
+
+});
